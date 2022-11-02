e8870c6baa4f36f084ebae496743c70c17865496
Liv Nelson <livnelson@me.com>
2022-11-01 21:32:10 -0700

    commit tree 48dd26ca66308d2369fc00e042a83838e3a07925

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(true)
+
+  const handleClick = () => {
+    // console.log("Click Worked!")
+    setIsInCart(!isInCart);
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{isInCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
